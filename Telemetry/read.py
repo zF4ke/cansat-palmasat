a0e91095ca0653ffddac01ed4ac0da3c582e3ff3
zF4ke <pedrohsilva955@gmail.com>
2022-03-15 19:27:23 +0000

    commands

--- Telemetry/read.py
+++ Telemetry/read.py
@@ -6,5 +6,5 @@ uart = serial.Serial(port='/dev/ttyS0', baudrate=9600)
 while True:
     uart.readline()
 
-    time.sleep(0.03)
+    time.sleep(1)
 
--- startup.py
+++ startup.py
@@ -7,9 +7,11 @@ sys.path.append(os.path.join(sys.path[0], 'BMP388'))
 
 from BMP388 import read_values as bmp
 
-def initMsg():
-    uart = serial.Serial(port='/dev/ttyS0', baudrate=9600)
+uart = serial.Serial(port='/dev/ttyS0', baudrate=9600)
 
+stop = False
+
+def initMsg():
     try:
         uart.write(str.encode('*** Starting ***\n'))
     except:
@@ -17,19 +19,40 @@ def initMsg():
 
     time.sleep(3)
 
+def stop():
+
+
+    pass
+
 def startBMP():
     try:
         uart.write(str.encode('\n\n: BMP388 is running.\n\n'))
     except:
         print('Something went wrong when writing to the serial port')
 
-
-    while True:
+    while not stop:
         bmp.run()
 
         time.sleep(0.1)
 
+def enableCommands():
+    while not stop:
+        command = uart.readline()
+
+        match command:
+            case 'stop':
+                stop = True
+            try:
+                uart.write(str.encode('*** Stopped ***\n'))
+            except:
+                print('Something went wrong when writing to the serial port')     
+            case _:        
+                return 0   # 0 is the default case if x is not found
+
+        time.sleep(0.5)
+
 if __name__ == '__main__':
     initMsg()
     startBMP()
+    enableCommands()
     
