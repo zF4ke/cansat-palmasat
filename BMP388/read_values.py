efce03092385de8cd9a8dbf734d675af4f613bcd
zF4ke <pedrohsilva955@gmail.com>
2022-04-30 11:25:12 +0100

    Add milliseconds

--- BMP388/read_values.py
+++ BMP388/read_values.py
@@ -6,18 +6,20 @@ from send_serial import write
 
 bmp = bmp388library.DFRobot_BMP388_I2C(0x77)
 
+
 def export_txt(output):
     now = datetime.now()
-    dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+    dt_string = now.strftime("%d/%m/%Y %H:%M:%S,%f")
 
     f = open("bmp_storage.txt", "a+")
 
     try:
         f.write(f'{dt_string} {output}\n')
     except:
-         print('Something went wrong when writing to the file')
+        print('Something went wrong when writing to the file')
     finally:
         f.close()
+
 
 def run():
     try:
@@ -31,4 +33,3 @@ def run():
 
     except:
         print("An exception occurred")
-
